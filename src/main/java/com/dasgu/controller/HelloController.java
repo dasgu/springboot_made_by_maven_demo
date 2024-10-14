9f1b85c82ac95aaade1f7ba72b33468e4f400656
dasgu <1875537406@qq.com>
2024-10-14 14:05:33 +0800

    third submit

--- src/main/java/com/dasgu/controller/HelloController.java
+++ src/main/java/com/dasgu/controller/HelloController.java
@@ -7,10 +7,19 @@ import java.util.HashMap;
 @RestController
 public class HelloController {
 
-    @RequestMapping(value = "/hi/{msg}", method = RequestMethod.GET)
-    Object hi(@PathVariable String msg) {
-        HashMap<String, Object> mp = new HashMap<>();
+    @RequestMapping(value = "/hi/msg={msg}&a={a}", method = RequestMethod.GET)
+    Object hi(@PathVariable String msg, @PathVariable Integer a) {
+        HashMap<Object, Object> mp = new HashMap<>();
         mp.put(msg, "hi~~");
+        mp.put(a, "int");
+        return mp;
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    Object hello(@RequestParam String msg, @RequestParam String a) {
+        HashMap<Object, Object> mp = new HashMap<>();
+        mp.put(msg, "hi~~");
+        mp.put(a, "int");
         return mp;
     }
 }
